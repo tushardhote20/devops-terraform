d3b234e331143d8728304f5e92f5e23ef582dc86
tushardhote20 <tushardhote2@gmail.com>
2025-12-19 17:58:43 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,2 @@
-output "ec2_id" { value = module.ec2.instance_id }
-output "vpc_id" { value = module.vpc.vpc_id }
+# output "ec2_id" { value = module.ec2.instance_id }
+# output "vpc_id" { value = module.vpc.vpc_id }
