53795f05d8ad3b33f8988a3085c23f1a096d7990
tushardhote20 <tushardhote2@gmail.com>
2025-12-21 23:00:17 +0530

    updated tfvar files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,10 +2,12 @@ resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
+  tags                 = var.tags
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
+  tags   = var.tags
 }
 
 resource "aws_subnet" "public" {
--- prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tusharbucket123.com"
-    key    = "app/flipkart/prod/terraform.tfstate"
-    region = "ap-south-1"
-    
-    #use_lockfile = "true"
-  }
-}
